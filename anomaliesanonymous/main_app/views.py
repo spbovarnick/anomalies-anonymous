d0d6676441fdc78dd19b20d0e08dbde860cf8919
gabewolford <gabewolford@gmail.com>
2023-04-02 13:20:53 -0700

    removed dummy data from views

--- anomaliesanonymous/main_app/views.py
+++ anomaliesanonymous/main_app/views.py
@@ -1,28 +1,6 @@
 from django.shortcuts import render
 from .models import Sighting, Comment
 
-sightings = [
-     {
-      "datetime": "1996-06-24T07:30:00.000Z",
-      "city": "Aurora",
-      "state": "CO",
-      "shape": "CHA",
-      "duration": 3600,
-      "description": "Obj. hovered 100 ft above car. Red blue lights on corners. Changed shape from cube to pyramid to triangle. Landed 800 ft away.",
-      "latitude": 39.7294444,
-      "longitude": -104.8313889
-    },
-    {
-      "datetime": "1998-10-10T09:30:00.000Z",
-      "city": "Hollywood",
-      "state": "CA",
-      "shape": "CHA",
-      "duration": 300,
-      "description": "I was standing outside on Sunset Blvd. at Vine and looked straight up which I normally do not do. I saw three bright white lights in a",
-      "latitude": 34.0983333,
-      "longitude": -118.3258333
-    },
-]
 
 # Create your views here.
 def home(request):
