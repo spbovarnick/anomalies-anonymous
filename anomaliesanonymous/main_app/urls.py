165cedbffbf71e426bb1be7290d76dc0609d9d3e
spbovarnick <spbovarnick@gmail.com>
2023-04-03 09:44:27 -0700

    fixed sightings_id typo in sightings_update url path

--- anomaliesanonymous/main_app/urls.py
+++ anomaliesanonymous/main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('sightings/', views.sightings_index, name='index'),
     path('sightings/<int:sighting_id>/', views.sightings_detail, name='detail'),
     path('sightings/create/', views.sightings_create, name='sightings_create'),
-    path('sightings/<int:pk>/update', views.sightings_update, name='sightings_update'),
+    path('sightings/<int:sighting_id>/update', views.sightings_update, name='sightings_update'),
     path('sightings/<int:pk>/delete', views.SightingDelete.as_view(), name='sightings_delete'),
 ]
