92ecadca88c866f3d7df9f178f96374235786a2b
spbovarnick <spbovarnick@gmail.com>
2023-04-02 20:36:14 -0700

    cleaned out detritus

--- anomaliesanonymous/main_app/forms.py
+++ anomaliesanonymous/main_app/forms.py
@@ -10,12 +10,10 @@ from .models import Sighting
 from django import forms
 
 # make sure to run pip3 install django-flatpickr !!!
-# also make sure to run pip3 install django-durationwidget !!!!
 class SightingForm(ModelForm):
     class Meta:
         model = Sighting
         fields = ['datetime', 'city', 'state', 'shape', 'duration', 'description']
-        # duration = forms.DurationField(widget=TimeDurationWidget(show_days=False), required=True)
         widgets = {
             'datetime': DateTimePickerInput(),
             'duration': TextInput(attrs={'placeholder': 'DD HH:MM:SS'})
